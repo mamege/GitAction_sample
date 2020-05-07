e135d733375cfeb1878dbf4f1be9ea63fbc72bd1
Your Name <you@example.com>
2020-05-07 23:38:14 +0900

    clickのオプション機能を試した． $ ./click_sample.py -f test ... test bash-4.2$ ./click_sample.py ... Set filename: ./click_sample.py -f <filename>

--- python_submodule/click_sample.py
+++ python_submodule/click_sample.py
@@ -5,8 +5,20 @@ import click
 
 @click.command()
 @click.argument('name', default='guest')
-def hello(name):
+
+# A. Option機能を試す．
+@click.option("-f", "--filename", "filename")
+
+def hello(name, filename):
     click.echo(f'Hello {name}')
+    # 普通に変数として使用できる．
+    print(name)
+    # A.
+    if filename == None :
+        print("Set filename: %s -f <filename>" % __file__)
+        exit(1)
+    else:
+        print(filename)
 
  
 if __name__ == '__main__':
